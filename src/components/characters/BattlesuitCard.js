ca15173018ecf97fd135f0acb2334a0ee4e152eb
Brian <briantcheung@gmail.com>
2023-04-07 13:56:53 -0700

    Added background colors

--- src/components/characters/BattlesuitCard.js
+++ src/components/characters/BattlesuitCard.js
@@ -2,6 +2,30 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import { Box, Typography } from "@mui/material";
 
+const BattlesuitBackground = (battlesuitType) => {
+    let color = "";
+    switch (battlesuitType) {
+        case "Biologic":
+            color = "rgb(255, 175, 44)";
+            break;
+        case "Psychic":
+            color = "rgb(255, 102, 204)";
+            break;
+        case "Mecha":
+            color = "rgb(44, 227, 255)";
+            break;
+        case "Quantum":
+            color = "rgb(160, 135, 255)";
+            break;
+        case "Imaginary":
+            color = "rgb(241, 212, 144)";
+            break;
+        default:
+            color = "rgb(192, 192, 192)";
+    }
+    return color;
+}
+
 const BattlesuitCard = (props) => {
 
     const theme = useTheme();
@@ -10,11 +34,16 @@ const BattlesuitCard = (props) => {
     return (
         <Box
             sx={{
-                border: `1px solid ${theme.border.color}`,
-                width: "132px",
+                width: "135px",
             }}
         >
-            <img src={(`${process.env.REACT_APP_URL}/characters/battlesuits/icons/${battlesuit.name.split(" ").join("_")}_(Icon).png`)} alt={battlesuit.name} />
+            <Box
+                sx={{
+                    backgroundColor: BattlesuitBackground(battlesuit.type),
+                }}
+            >
+                <img src={(`${process.env.REACT_APP_URL}/characters/battlesuits/icons/${battlesuit.name.split(" ").join("_")}_(Icon).png`)} alt={battlesuit.name} />
+            </Box>
             <Typography sx={`${theme.font}`}>{battlesuit.name}</Typography>
         </Box>
     )
