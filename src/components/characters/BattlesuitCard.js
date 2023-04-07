7a67fb7f00679ba9c2b1473a5f88a128dcdfe627
Brian <briantcheung@gmail.com>
2023-04-07 02:55:13 -0700

    Added images to card

--- src/components/characters/BattlesuitCard.js
+++ src/components/characters/BattlesuitCard.js
@@ -1,13 +1,16 @@
 import * as React from "react";
+import { useTheme } from "@mui/material/styles";
 import { Box, Typography } from "@mui/material";
 
 const BattlesuitCard = (props) => {
 
-    let {battlesuit} = props
+    const theme = useTheme();
+    let { battlesuit } = props;
 
     return (
         <Box>
-            <Typography>{battlesuit.name}</Typography>
+            <img src={(`${process.env.REACT_APP_URL}/characters/battlesuits/icons/${battlesuit.name.split(" ").join("_")}_(Icon).png`)} alt={battlesuit.name} />
+            <Typography sx={`${theme.font}`}>{battlesuit.name}</Typography>
         </Box>
     )
 
