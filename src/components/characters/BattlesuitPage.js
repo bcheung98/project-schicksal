07005a55325cae7c2ad15ab6cf14b64d3bb9bc9a
Brian <briantcheung@gmail.com>
2023-04-07 18:09:25 -0700

    Added basic page layout

--- src/components/characters/BattlesuitPage.js
+++ src/components/characters/BattlesuitPage.js
@@ -3,9 +3,11 @@ import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const BattlesuitPage = (props) => {
 
+    const theme = useTheme();
     let { character_name, battlesuit_name } = useParams();
     let { characters } = props.characters;
     let character = characters.find(char => char.character.split(" ").join("_") === character_name);
@@ -17,9 +19,58 @@ const BattlesuitPage = (props) => {
     if (battlesuit !== undefined) {
         let { name, type, rank } = battlesuit;
         return (
-            <Box>
-                <Typography>{name}</Typography>
-            </Box>
+            <React.Fragment>
+                <Grid container sx={{ mb: "20px" }}>
+                    <Grid xs="auto">
+                        <Box sx={{ position: "absolute", left: "20px" }}>
+                            <img src={`${process.env.REACT_APP_URL}/ranks/Valkyrie_${rank}.png`} alt={rank} />
+                        </Box>
+                        <img src={`${process.env.REACT_APP_URL}/characters/battlesuits/images/${name.split(" ").join("_")}.png`} alt={name}
+                            style={{
+                                width: "25vw",
+                                height: "660px",
+                                objectFit: "cover",
+                                marginLeft: "15px",
+                                borderLeft: `1px solid ${theme.border.color}`,
+                                borderRight: `1px solid ${theme.border.color}`,
+                                borderBottom: `1px solid ${theme.border.color}`,
+                                borderRadius: "0px 0px 5px 5px",
+                            }}
+                        />
+                    </Grid>
+                    <Grid xs>
+                        <Box sx={{ display: "flex" }}>
+                            <img src={`${process.env.REACT_APP_URL}/types/${type}.png`} alt={type}
+                                style={{
+                                    height: "72px",
+                                    width: "72px",
+                                    marginLeft: "25px",
+                                    marginTop: "25px",
+                                }}
+                            />
+                            <Box sx={{ ml: "25px" }}>
+                                <Typography
+                                    variant="h6"
+                                    sx={{
+                                        mt: "15px",
+                                        fontStyle: "italic",
+                                    }}
+                                >
+                                    {character.character}
+                                </Typography>
+                                <Typography
+                                    variant="h3"
+                                    sx={{
+                                        fontWeight: "bold"
+                                    }}
+                                >
+                                    {battlesuit.displayName ? battlesuit.displayName : name}
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </React.Fragment>
         )
     }
 }
