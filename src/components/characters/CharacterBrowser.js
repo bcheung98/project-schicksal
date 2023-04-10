c2bc117520011edf12b0e93011316d006d7174f8
Brian <briantcheung@gmail.com>
2023-04-10 02:01:49 -0700

    Battlesuits of the same character now on the same line

--- src/components/characters/CharacterBrowser.js
+++ src/components/characters/CharacterBrowser.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import BattlesuitCard from "./BattlesuitCard";
+import { Box } from "@mui/material";
 
 const CharacterBrowser = (props) => {
 
@@ -10,13 +11,11 @@ const CharacterBrowser = (props) => {
         <React.Fragment>
             {characters.characters.length > 0 &&
                 <React.Fragment>
-                    {characters.characters.map(char => {
-                        return (
-                            <React.Fragment key={char.character}>
-                                {char.battlesuits.map(battlesuit => <BattlesuitCard key={battlesuit.name} character={char.character} battlesuit={battlesuit} />)}
-                            </React.Fragment>
-                        )
-                    })}
+                    {characters.characters.map(char => (
+                        <Box key={char.character} sx={{ display: "flex" }}>
+                            {char.battlesuits.map(battlesuit => <BattlesuitCard key={battlesuit.name} character={char.character} battlesuit={battlesuit} />)}
+                        </Box>
+                    ))}
                 </React.Fragment>
             }
         </React.Fragment>
